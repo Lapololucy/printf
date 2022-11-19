e6deb6aa55cf972ee8474589fb83c036fb2b13a4
Lapololucy <lapololucy@gmail.com>
2022-11-19 09:24:46 -0800

    TASKING

--- main.h
+++ main.h
@@ -37,7 +37,6 @@ struct fmt
  */
 typedef struct fmt fmt_t;
 
-int _putchar(char c);
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
