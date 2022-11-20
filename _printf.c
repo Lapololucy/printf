008059a1bdc98071ad0b27e08de05b96ed1fe047
MoAfifi0 <m.afifi00000gmail.com>
2022-11-19 23:45:03 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -37,7 +37,8 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
